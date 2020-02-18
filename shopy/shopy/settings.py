2a167a9c5d4f7855195114748dad536dde39026b
Denis <schmelev94@gmail.com>
2020-02-19 00:59:59 +0300

    Added authnapp

--- shopy/shopy/settings.py
+++ shopy/shopy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authnapp',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Auth model
+AUTH_USER_MODEL = 'authnapp.ShopUser'
